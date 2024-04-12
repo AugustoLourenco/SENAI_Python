6c03828221612c74fc2fc391f8e0084f97e59371
AugustoLourenco <augusto.p.f.lourenco@gmail.com>
2024-04-12 16:46:01 -0300

    Aula_07_Tuplas

--- Aula_07_Tuplas/desafio_05.py
+++ Aula_07_Tuplas/desafio_05.py
@@ -1,7 +1,8 @@
-produtos = ('Sofa', 800.00, 'Geladeira', 700.00, 'Fogão', 700.00)
+# DESAFIO 05 - Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
+produtos = ('Sofa' , 800.00 , 'Geladeira' , 700.00, 'Fogão' , 700.00 , 'Forno' , 300.00)
 tamanho = len(produtos)
 
-
-
 for i in range (0, tamanho, 2):
-    print(f"{produtos[i]:.<30}")
+    print(f"{produtos[i]:.<30}....R${produtos[i + 1]}")
---
+++ Aula_07_Tuplas/desafio_05_part02.py
@@ -0,0 +1,9 @@
+#No terminal( para instalar a biblioteca): pip install tabulate
+
+# from tabulate import tabulate
+
+# produtos = [["Bala","0.30"],["suco","4.10"],["Sorvete","32.30"],["Paçoca","1.5"],["MeM","11.30"],["Milka","18.30"]]
+
+# tpProduto = tuple(produtos)
+           
+# print(tabulate(tpProduto, headers=["Produto", "Preço"]))
